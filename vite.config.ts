d03c52f8e1d7544a285522464c5621d097c6e702
rogertgu <inforogert@gmail.com>
2025-07-25 22:13:56 +0200

    added to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/formulario-examen/',
+  base: '/sheet-to-scan-form/',
   server: {
     host: "::",
     port: 8080,
